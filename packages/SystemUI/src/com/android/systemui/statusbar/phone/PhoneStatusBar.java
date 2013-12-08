9370660fe685420b31dc1a28c07c45260ba7a7b7
Daniel MM <daxx4182@gmail.com>
2013-12-09 07:07:39 +0800

    Status icon color (1/2)
    This will override the following system icons:
    
    * Status bar icons
    ** Airplane
    ** Wifi
    ** Bluetooth
    ** Signal
    * Battery paint color
    * Clock text color
    
    And it will not override the, but wish to do:
    
    * Notification icon color and opacity (maybe adding it soon)
    
    PS2: Added support for Battery
    PS3: Add missing variables
    PS4: Fix color resources when you're in a theme or whenever a user switch their status icon drawables
    PS5: Masking battery color
    PS6: Derp fixes
    PS7: Remove percentage icon color support for now (Will re add it in the next patch)
    PS8: Fix the UserHandle
    PS9: Fix the frame color for normal battery color
    PS10: No need to reboot to change the icon color and text, battery.
    
    Credits:
    
    * Partially of the code and idea from ParanoidAndroid
    
    Change-Id: I37cc69109deab8c9e065245445b83d15ba16df5f
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/cm/DevUtils.java
    	packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -60,7 +60,7 @@ import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
-import android.graphics.PorterDuff;
+import android.graphics.PorterDuff.Mode;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
@@ -416,6 +416,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private BatteryMeterView mBattery;
     private BatteryCircleMeterView mCircleBattery;
 
+    private boolean mCustomColor;
+    private int systemColor;
+
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
         ? new GestureRecorder("/sdcard/statusbar_gestures.dat")
@@ -510,6 +513,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_SHORTCUTS_COLOR_MODE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.CUSTOM_SYSTEM_ICON_COLOR), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SYSTEM_ICON_COLOR), false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_CAMERA_WIDGET),
                     false, this, UserHandle.USER_ALL);
@@ -722,6 +729,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
                 updateCarrierAndWifiLabelVisibility(false);
             }
+
+            mCustomColor = Settings.System.getIntForUser(resolver,
+                    Settings.System.CUSTOM_SYSTEM_ICON_COLOR, 0, UserHandle.USER_CURRENT) == 1;
+            systemColor = Settings.System.getIntForUser(resolver,
+                    Settings.System.SYSTEM_ICON_COLOR, -2, UserHandle.USER_CURRENT);
             updateBatteryIcons();
 
             mFlipInterval = Settings.System.getIntForUser(mContext.getContentResolver(),
@@ -1934,6 +1946,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     public void addIcon(String slot, int index, int viewIndex, StatusBarIcon icon) {
         if (SPEW) Log.d(TAG, "addIcon slot=" + slot + " index=" + index + " viewIndex=" + viewIndex
                 + " icon=" + icon);
+
+        Drawable iconDrawable = StatusBarIconView.getIcon(mContext, icon);
+        if (mCustomColor) {
+            iconDrawable.setColorFilter(systemColor, Mode.SRC_ATOP);
+        } else {
+            iconDrawable.clearColorFilter();
+        }
+
         StatusBarIconView view = new StatusBarIconView(mContext, slot, null);
         view.set(icon);
         mStatusIcons.addView(view, viewIndex, new LinearLayout.LayoutParams(mIconSize, mIconSize));
@@ -1943,6 +1963,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             StatusBarIcon old, StatusBarIcon icon) {
         if (SPEW) Log.d(TAG, "updateIcon slot=" + slot + " index=" + index + " viewIndex=" + viewIndex
                 + " old=" + old + " icon=" + icon);
+
+        Drawable iconDrawable = StatusBarIconView.getIcon(mContext, icon);
+        if (mCustomColor) {
+            iconDrawable.setColorFilter(systemColor, Mode.SRC_ATOP);
+        } else {
+            iconDrawable.clearColorFilter();
+        }
+
         StatusBarIconView view = (StatusBarIconView)mStatusIcons.getChildAt(viewIndex);
         view.set(icon);
     }
@@ -4512,7 +4540,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         @Override
         public void draw(Canvas canvas) {
-            canvas.drawColor(mColor, PorterDuff.Mode.SRC);
+            canvas.drawColor(mColor, Mode.SRC);
         }
 
         @Override
