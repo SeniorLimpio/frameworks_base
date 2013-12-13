24bcbf5933984cfa262dc02a4d16512a536b8f80
bart452 <bart_96@live.nl>
2013-12-13 21:45:54 +0100

    base: quicksettings: allow flip animation on clicking a tile (1/2)
    ported from https://github.com/BeerGang/android_frameworks_base/commit/ba144b2defd209c2c020366316c65b288bf68880
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/quicksettings/LteTile.java
    
    Change-Id: Ic5e3b6c1eee902fd59c1d54ac6c5efb8cffad4ba
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/quicksettings/AirplaneModeTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/PerformanceProfileTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/TorchTile.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2908,6 +2908,13 @@ public final class Settings {
          */
         public static final String HALO_ENABLED = "halo_enabled";
 
+       /**
+         * Animate-flip Quick Settings Panel Tiles on click
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
+
         /**
          * HALO, should default to 0 (no, do not show)
          * @hide
