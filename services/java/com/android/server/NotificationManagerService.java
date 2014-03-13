50845f0501b13fa17f8584cc937bdee1e482d965
SeniorLimpio <seniorlimpio1@gmail.com>
2014-03-13 14:10:15 +1300

    Fix build

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -649,10 +649,9 @@ public class NotificationManagerService extends INotificationManager.Stub
         for (NotificationListenerInfo info : toRemove) {
             final ComponentName component = info.component;
             final int oldUser = info.userid;
-            if (!info.isSystem) {
-                Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);
-                unregisterListenerService(component, info.userid);
-            }
+            Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);
+            unregisterListenerService(component, info.userid);
+           
         }
 
         final int N = toAdd.size();
@@ -673,11 +672,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     @Override
     public void registerListener(final INotificationListener listener,
             final ComponentName component, final int userid) {
-        final int permission = mContext.checkCallingPermission(
-                android.Manifest.permission.SYSTEM_NOTIFICATION_LISTENER);
-	if (permission == PackageManager.PERMISSION_DENIED)
             checkCallerIsSystem();
-
         synchronized (mNotificationList) {
             try {
                 NotificationListenerInfo info
