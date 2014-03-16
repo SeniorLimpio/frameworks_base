b3282664f4941bff34ff280dd1811bd670216761
SeniorLimpio <seniorlimpio1@gmail.com>
2014-03-16 21:16:01 +1300

    Fix Development Settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5865,12 +5865,6 @@ public final class Settings {
         public static final String DEVELOPMENT_SHORTCUT = "development_shortcut";
 
         /**
-         * Whether newly installed apps should run with privacy guard by default
-         * @hide
-         */
-         public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
-
-        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -5923,8 +5917,8 @@ public final class Settings {
             UI_THEME_AUTO_MODE,
             PRIVACY_GUARD_DEFAULT,
             PRIVACY_GUARD_NOTIFICATION,
+            DEVELOPMENT_SHORTCUT,
             ADVANCED_REBOOT
-            DEVELOPMENT_SHORTCUT
         };
 
         /**
