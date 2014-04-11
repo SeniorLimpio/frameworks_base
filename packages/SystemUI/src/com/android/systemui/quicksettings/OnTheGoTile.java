Basti1337/android_frameworks_base-1

Squashed On-The-Go mode (1/2)
On-The-Go turns on the camera of your phone and streams it to an overlay,
which is drawn over everything.
this makes you feel like your display is transparent, as you see the content
behind it.

its really useful when you are using your phone while walking, thats why
we called it "On-The-Go"

allows to change transparency of the overlay via the notification.

Modified parts of the code to make working for FusionSP
Enable/disable with QS tile, longpress on tile for options.

Credits: NamelessRom

Signed-off-by: dragon <dragon7780@hotmail.nl>

packages/SystemUI/src/com/android/systemui/quicksettings/OnTheGoTile.java
@@ -1,0 +1,130 @@
+/*
+* <!--
+*    Copyright (C) 2014, The Fusion Project
+*    Credits: The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.systemui.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+import com.android.internal.util.fusion.FusionActions;
+import com.android.internal.util.fusion.FusionUtils;
+import com.android.systemui.R;
+import com.android.systemui.fusion.onthego.OnTheGoDialog;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+
+public class OnTheGoTile extends QuickSettingsTile {
+
+    private static final int CAMERA_BACK  = 0;
+    private static final int CAMERA_FRONT = 1;
+
+    public OnTheGoTile(final Context context, final QuickSettingsController qsc) {
+        super(context, qsc);
+
+        mOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FusionActions.processAction(mContext, FusionActions.ACTION_ONTHEGO_TOGGLE);
+            }
+        };
+
+        mOnLongClick = new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                new OnTheGoDialog(mContext).show();
+                return true;
+            }
+        };
+
+        qsc.registerObservedContent(Settings.System.getUriFor(
+                Settings.System.ON_THE_GO_CAMERA), this);
+    }
+
+    @Override
+    void onPostCreate() {
+        updateTile();
+        super.onPostCreate();
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile();
+        super.updateResources();
+    }
+
+    private void toggleCamera() {
+        final ContentResolver resolver = mContext.getContentResolver();
+        final int camera = Settings.System.getInt(resolver,
+                Settings.System.ON_THE_GO_CAMERA,
+                CAMERA_BACK);
+
+        int newValue;
+        if (camera == CAMERA_BACK) {
+            newValue = CAMERA_FRONT;
+        } else {
+            newValue = CAMERA_BACK;
+        }
+
+        Settings.System.putInt(resolver,
+                Settings.System.ON_THE_GO_CAMERA,
+                newValue);
+
+        updateResources();
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateResources();
+    }
+
+    private synchronized void updateTile() {
+
+        int cameraMode;
+
+        if (FusionUtils.hasFrontCamera(mContext)) {
+            cameraMode = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.ON_THE_GO_CAMERA,
+                    CAMERA_BACK);
+        } else {
+            cameraMode = CAMERA_BACK;
+        }
+
+        switch (cameraMode) {
+            default:
+            case CAMERA_BACK:
+                mLabel = mContext.getString(R.string.quick_settings_onthego_back);
+                mDrawable = R.drawable.ic_qs_onthego;
+                break;
+            case CAMERA_FRONT:
+                mLabel = mContext.getString(R.string.quick_settings_onthego_front);
+                mDrawable = R.drawable.ic_qs_onthego_front;
+                break;
+        }
+
+    }
+
+}
+

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -54,6 +54,7 @@ import static com.android.internal.util.fusion.QSConstants.TILE_WIFI;
 import static com.android.internal.util.fusion.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.fusion.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.fusion.QSConstants.TILE_INTERNALMEMORY;
+import static com.android.internal.util.fusion.QSConstants.TILE_ONTHEGO;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -107,6 +108,7 @@ import com.android.systemui.quicksettings.VolumeTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
+import com.android.systemui.quicksettings.OnTheGoTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -114,7 +116,7 @@ import java.util.HashSet;
 import java.util.List;
 
 public class QuickSettingsController {
-    private static String TAG = "QuickSettingsController";
+    private static final String TAG = "QuickSettingsController";
 
     // Stores the broadcast receivers and content observers
     // quick tiles register for.
@@ -260,6 +262,8 @@ public class QuickSettingsController {
                 qs = new ContactTile(mContext, this, findCustomKey(tile));
             } else if (tile.equals(TILE_INTERNALMEMORY)) {
                 qs = new InternalMemory(mContext, this);
+            } else if (tile.equals(TILE_ONTHEGO)) {
+                qs = new OnTheGoTile(mContext, this);     
             }
 
             if (qs != null) {

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -74,6 +74,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.internal.app.ThemeUtils;
+import com.android.internal.util.fusion.FusionActions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -410,6 +411,34 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
         }
 
+        // next: On-The-Go, if enabled
+        boolean showOnTheGo = Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.POWER_MENU_ONTHEGO_ENABLED, false);
+        if (showOnTheGo) {
+            mItems.add(
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego,
+                        R.string.global_action_onthego) {
+
+                        public void onPress() {
+                            FusionActions.processAction(mContext,
+                                    FusionActions.ACTION_ONTHEGO_TOGGLE);
+                        }
+
+                        public boolean onLongPress() {
+                            return false;
+                        }
+
+                        public boolean showDuringKeyguard() {
+                            return true;
+                        }
+
+                        public boolean showBeforeProvisioning() {
+                            return true;
+                        }
+                    }
+            );
+        }
+
         // one more thing: optionally add a list of users to switch to
         if (SystemProperties.getBoolean("fw.power_user_switcher", false)) {
             addUsersToMenu(mItems);
@@ -606,6 +635,15 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
         }
     }
+
+    //private void startOnTheGo() {
+      //  final ComponentName cn = new ComponentName("com.android.systemui",
+        //        "com.android.systemui.fusion.onthego.OnTheGoService");
+        //final Intent startIntent = new Intent();
+        //startIntent.setComponent(cn);
+        //startIntent.setAction("start");
+        //mContext.startService(startIntent);
+    //}
 
     private void prepareDialog() {
         refreshSilentMode();

SeniorLimpio/frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	core/res/res/values/fusion_symbols.xml
	packages/SystemUI/res/values/slim_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
	policy/src/com/android/internal/policy/impl/GlobalActions.java

packages/SystemUI/src/com/android/systemui/quicksettings/OnTheGoTile.java
@@ -1,0 +1,130 @@
+/*
+* <!--
+*    Copyright (C) 2014, The Fusion Project
+*    Credits: The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.systemui.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+import com.android.internal.util.limpio.LimpioActions;
+import com.android.internal.util.limpio.LimpioUtils;
+import com.android.systemui.R;
+import com.android.systemui.fusion.onthego.OnTheGoDialog;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+
+public class OnTheGoTile extends QuickSettingsTile {
+
+    private static final int CAMERA_BACK  = 0;
+    private static final int CAMERA_FRONT = 1;
+
+    public OnTheGoTile(final Context context, final QuickSettingsController qsc) {
+        super(context, qsc);
+
+        mOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FusionActions.processAction(mContext, FusionActions.ACTION_ONTHEGO_TOGGLE);
+            }
+        };
+
+        mOnLongClick = new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                new OnTheGoDialog(mContext).show();
+                return true;
+            }
+        };
+
+        qsc.registerObservedContent(Settings.System.getUriFor(
+                Settings.System.ON_THE_GO_CAMERA), this);
+    }
+
+    @Override
+    void onPostCreate() {
+        updateTile();
+        super.onPostCreate();
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile();
+        super.updateResources();
+    }
+
+    private void toggleCamera() {
+        final ContentResolver resolver = mContext.getContentResolver();
+        final int camera = Settings.System.getInt(resolver,
+                Settings.System.ON_THE_GO_CAMERA,
+                CAMERA_BACK);
+
+        int newValue;
+        if (camera == CAMERA_BACK) {
+            newValue = CAMERA_FRONT;
+        } else {
+            newValue = CAMERA_BACK;
+        }
+
+        Settings.System.putInt(resolver,
+                Settings.System.ON_THE_GO_CAMERA,
+                newValue);
+
+        updateResources();
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateResources();
+    }
+
+    private synchronized void updateTile() {
+
+        int cameraMode;
+
+        if (FusionUtils.hasFrontCamera(mContext)) {
+            cameraMode = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.ON_THE_GO_CAMERA,
+                    CAMERA_BACK);
+        } else {
+            cameraMode = CAMERA_BACK;
+        }
+
+        switch (cameraMode) {
+            default:
+            case CAMERA_BACK:
+                mLabel = mContext.getString(R.string.quick_settings_onthego_back);
+                mDrawable = R.drawable.ic_qs_onthego;
+                break;
+            case CAMERA_FRONT:
+                mLabel = mContext.getString(R.string.quick_settings_onthego_front);
+                mDrawable = R.drawable.ic_qs_onthego_front;
+                break;
+        }
+
+    }
+
+}
+

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui.statusbar.phone;
 
+<<<<<<< HEAD
 import static com.android.internal.util.slim.QSConstants.TILES_DEFAULT;
 import static com.android.internal.util.slim.QSConstants.DYNAMIC_TILES_DEFAULT;
 import static com.android.internal.util.slim.QSConstants.TILE_AIRPLANE;
@@ -53,6 +54,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -105,6 +107,7 @@ import com.android.systemui.quicksettings.VolumeTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
+import com.android.systemui.quicksettings.OnTheGoTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -112,7 +115,7 @@ import java.util.HashSet;
 import java.util.List;
 
 public class QuickSettingsController {
-    private static String TAG = "QuickSettingsController";
+    private static final String TAG = "QuickSettingsController";
 
     // Stores the broadcast receivers and content observers
     // quick tiles register for.
@@ -256,6 +259,8 @@ public class QuickSettingsController {
                 qs = new CustomTile(mContext, this, findCustomKey(tile));
             } else if (tile.contains(TILE_CONTACT)) {
                 qs = new ContactTile(mContext, this, findCustomKey(tile));
+            } else if (tile.equals(TILE_ONTHEGO)) {
+                qs = new OnTheGoTile(mContext, this);     
             }
 
             if (qs != null) {

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -73,6 +73,9 @@ import android.widget.ImageView.ScaleType;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.internal.app.ThemeUtils;
+import com.android.internal.util.limpio.LimpioActions;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -397,6 +400,34 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
         }
 
+        // next: On-The-Go, if enabled
+        boolean showOnTheGo = Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.POWER_MENU_ONTHEGO_ENABLED, false);
+        if (showOnTheGo) {
+            mItems.add(
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego,
+                        R.string.global_action_onthego) {
+
+                        public void onPress() {
+                            LimpioActions.processAction(mContext,
+                                    LimpioActions.ACTION_ONTHEGO_TOGGLE);
+                        }
+
+                        public boolean onLongPress() {
+                            return false;
+                        }
+
+                        public boolean showDuringKeyguard() {
+                            return true;
+                        }
+
+                        public boolean showBeforeProvisioning() {
+                            return true;
+                        }
+                    }
+            );
+        }
+
         // one more thing: optionally add a list of users to switch to
         if (SystemProperties.getBoolean("fw.power_user_switcher", false)) {
             addUsersToMenu(mItems);
@@ -593,6 +624,15 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
         }
     }
+
+    //private void startOnTheGo() {
+      //  final ComponentName cn = new ComponentName("com.android.systemui",
+        //        "com.android.systemui.limpio.onthego.OnTheGoService");
+        //final Intent startIntent = new Intent();
+        //startIntent.setComponent(cn);
+        //startIntent.setAction("start");
+        //mContext.startService(startIntent);
+    //}
 
     private void prepareDialog() {
         refreshSilentMode();