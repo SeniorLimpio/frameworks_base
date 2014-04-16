bf21c31ea5c65a328c53876c463f687ee1e42b2e
D4rKn3sSyS <jesdga95@gmail.com>
2014-04-16 21:09:05 +0200

    Base: Floating window with multitasking
    basic floating window are taken from AOSPA (HALO) by @D4rKn3sSyS
    
    Drawables courtesy of @Alex Cruz thanks to him :)
    
    with heavy change to get this work for multitasking
    
    this feature, has ability to:
    1. Moveable window to various position
    2. Scaling window with 2 finger like zoom in/out
    3. Double tap in actionbar or above window to fullscreen or back to floating window
    4. Automatically resize when Snapping into one of side (Top, Bottom, Left, Right)
       (taken from xhalofloatingwindow by @zst123)
    5. Automatically switch focus from other app (taken from multiwindow by @xplodwild)
    6. Unlimited floating window app (depends on RAM)
    
    this is still under testing, to make fully works
    so if there any bug because of this, DO NOT REPORT INTO OMNIROM JIRA
    
    to make app launch using Floating window feature
    
    you must add Intent.FLAG_FLOATING_WINDOW
    before start an activity
    
    for example you can look at https://gerrit.omnirom.org/6515
    
    need https://gerrit.omnirom.org/#/c/6561
    
    PS.17 : fix scaling
    
    Change-Id: Iad00c239de7d66e8bb45205ce626d4f08417b7cd
    
    Conflicts:
    	core/java/android/app/IActivityManager.java
    	core/java/android/content/Intent.java
    	core/java/android/view/IWindowManager.aidl
    	core/res/res/values/strings_custom.xml
    	core/res/res/values/symbols_custom.xml
    	services/java/com/android/server/wm/WindowManagerService.java
    
    Conflicts:
    	core/java/android/app/Activity.java
    
    Conflicts:
    	core/res/res/values/liquid_strings.xml
    	core/res/res/values/liquid_symbols.xml

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3530,6 +3530,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
+
     /** {@inheritDoc} */
     @Override
     public void layoutWindowLw(WindowState win, WindowManager.LayoutParams attrs,
