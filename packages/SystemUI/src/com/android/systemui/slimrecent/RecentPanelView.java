51826ddd32a758be209811e975f769c8c40ecfe5
lion0738 <lion0738@naver.com>
2014-04-25 22:58:29 +0900

    SlimRecents: add floating window menu
    Change-Id: I63894fb8551b881c45c991bc30353b3dc4f3e43c

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -325,6 +325,8 @@ public class RecentPanelView {
             }
         }
 
+        popup.getMenu().add(0, MENU_APP_FLOATING_ID, 0,
+                mContext.getResources().getString(R.string.status_bar_recent_floating_item_title));
 
         if (Settings.Secure.getInt(mContext.getContentResolver(),
                 Settings.Secure.DEVELOPMENT_SHORTCUT, 0) == 1) {
@@ -362,6 +364,19 @@ public class RecentPanelView {
             public boolean onMenuItemClick(MenuItem item) {
                 if (item.getItemId() == MENU_APP_DETAILS_ID) {
                     startApplicationDetailsActivity(td.packageName, null, null);
+                } else if (item.getItemId() == MENU_APP_FLOATING_ID) {
+                    Intent transparent = new Intent(mContext, com.android.systemui.Transparent.class);
+                    transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                            | Intent.FLAG_FLOATING_WINDOW
+                            | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    mContext.startActivity(transparent);
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                            | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            | Intent.FLAG_FLOATING_WINDOW);
+                    intent.setComponent(td.intent.getComponent());
+                    mContext.startActivity(intent);
+                    exit();
                 } else if (item.getItemId() == MENU_APP_STOP_ID) {
                     ActivityManager am = (ActivityManager)mContext.getSystemService(
                             Context.ACTIVITY_SERVICE);
