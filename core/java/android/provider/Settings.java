55a6a52210eb2554da70519682c5330e2e7d9b02
Cristiano Matos <cristianobmatos@gmail.com>
2014-05-02 12:19:26 -0300

    PEEK: configurable pickup timeout (1/2)
    - Allow the user to set different pickup timeout when a notification comes up through PEEK.
    
    Change-Id: Ifcd2f13101dcf6973ebcf016bce5f5314e1ce04c
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationPeek.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/Peek.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5040,6 +5040,12 @@ public final class Settings {
         public static final String PEEK_TIME = "peek_time";
 
         /**
+         * Set the timeout of peek when pikcing up the device
+         * @hide
+         */
+        public static final String PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
+
+        /**
          * Whether or not to use the app sidebar
          *
          * @hide
