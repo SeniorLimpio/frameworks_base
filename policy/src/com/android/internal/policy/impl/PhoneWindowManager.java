b49a0deeb9d2392fa13db46e4f79c4fa44fd55c5
liquid0624 <rjbirnesser@gmail.com>
2014-05-26 00:51:36 -0400

    Remove some theme chooser leftovers

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5033,12 +5033,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            mUiContext = null;
-        }
-    };
-
     @Override
     public void screenTurnedOff(int why) {
         EventLog.writeEvent(70000, 0);
