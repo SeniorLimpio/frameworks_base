80884aff279e7ea98492a724d98913f6920bb766
Ramjee Singh <ramjee@codeaurora.org>
2014-05-26 11:04:27 +0530

    Revert "audio: Fixed dead lock in AudioService during volume change"
    This reverts commit 2d99ad7f7f4bc2f9124a4fe505c2f8ea699ae74e.
    
    Change-Id: I7c6f288b5761618ac13cd0d275e5d45c9f177c50

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -3250,7 +3250,7 @@ public class AudioService extends IAudioService.Stub {
                             device);
         }
 
-        public boolean setIndex(int index, int device) {
+        public synchronized boolean setIndex(int index, int device) {
             int oldIndex = getIndex(device);
             index = getValidIndex(index);
             synchronized (mCameraSoundForced) {
@@ -3258,9 +3258,8 @@ public class AudioService extends IAudioService.Stub {
                     index = mIndexMax;
                 }
             }
-            synchronized (this) {
-                mIndex.put(device, index);
-            }
+            mIndex.put(device, index);
+
             if (oldIndex != index) {
                 // Apply change to all streams using this one as alias
                 // if changing volume of current device, also change volume of current
