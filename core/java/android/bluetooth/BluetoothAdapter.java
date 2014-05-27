06d5e0d48d84243c967dcf447ba4c1ded2e8a9e6
Jorge Ruesga <jorge@ruesga.com>
2014-05-27 20:22:23 +0200

    appops: enforce appop for bluetooth disabled action
    Bluetooth disable action should be enforced in the same manner that the enable one.
    
    Patchset 2: Fix derp
    Patchset 3: Fix build
    Patchset 4: Rebase. Fix comment style
    Patchset 5: Rebase
    
    Change-Id: Ie161ac0c038acbd4c1ce7f27ab4c5897813fd4cf
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -711,10 +711,7 @@ public final class BluetoothAdapter {
      *         immediate error
      */
     public boolean disable() {
-        try {
-            return mManagerService.disable(true);
-        } catch (RemoteException e) {Log.e(TAG, "", e);}
-        return false;
+        return disable(true);
     }
 
     /**
@@ -730,7 +727,7 @@ public final class BluetoothAdapter {
     public boolean disable(boolean persist) {
 
         try {
-            return mManagerService.disable(persist);
+            return mManagerService.disable(ActivityThread.currentPackageName(), persist);
         } catch (RemoteException e) {Log.e(TAG, "", e);}
         return false;
     }
