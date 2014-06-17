f8595ffc373c6c0ef794ac6a642a5992afd94c2e
cristianomatos <cristianobmatos@gmail.com>
2014-06-17 07:15:57 -0300

    Hover notification timeout (1/2)
    Allow user to set how much time the hover notification will stay visisible without touch the screen
    
    Change-Id: I5847adb3f33522d9d78867685f9867477542c37e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4934,6 +4934,13 @@ public final class Settings {
         public static final String HOVER_REQUIRE_FULLSCREEN_MODE = "hover_require_fullscreen_mode";
 
         /**
+         * Hover: long fade out delay, default is 5000ms (5s).
+         *
+         * @hide
+         */
+        public static final String HOVER_LONG_FADE_OUT_DELAY = "hover_long_fade_out_delay";
+
+        /**
          * Hover: Exclude non-clearable notifications, default is 0 (off).
          *
          * @hide
