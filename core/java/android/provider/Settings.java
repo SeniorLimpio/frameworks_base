fe02537e6928c1a406d430625af63c5f302fa8a5
Daniel Koman <dankoman30@gmail.com>
2014-06-26 02:53:30 +0200

    FB: Slimrecents background color && transparency [2/2]
    PS: now setting sticks through reboots and TRDS state
            changes. also, add reset to default color option.
    
    PS: when resetting to default, use TRDS (png) resources
            instead of custom color
    
    PS: remove LSN dependency
    
    PS: make default background setting cognizant of
            recent panel gravity setting to display
            correct background resource
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
    
    Change-Id: I5c81db94edb37ccdbd4943da11de9a277d6fe0fe
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5242,6 +5242,13 @@ public final class Settings {
         public static final String SCREEN_ON_NOTIFICATION_LED = "screen_on_notification_led";
 
         /**
+         * Recent panel background color
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
