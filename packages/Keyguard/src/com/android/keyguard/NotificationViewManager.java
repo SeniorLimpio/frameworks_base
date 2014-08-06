shaheerkv/android_frameworks_base

Frameworks: hide heads up when screen is on
This hides heads up when lock screen notifications are
enabled and notifcations are recieved while screen is on

Change-Id: Ie0193b51a86b0443def12d90dd6d2c9cdc677606

packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -37,11 +37,11 @@ import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.util.liquid.QuietHoursHelper;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.android.internal.util.liquid.QuietHoursHelper;
 
 public class NotificationViewManager {
     private final static String TAG = "Keyguard:NotificationViewManager";
@@ -77,6 +77,7 @@ public class NotificationViewManager {
         public boolean expandedView = true;
         public boolean forceExpandedView = false;
         public boolean wakeOnNotification = false;
+        public boolean disableHeadsup = false;
         public int notificationsHeight = 4;
         public float offsetTop = 0.3f;
         public boolean privacyMode = false;
@@ -106,6 +107,8 @@ public class NotificationViewManager {
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_FORCE_EXPANDED_VIEW), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_WAKE_ON_NOTIFICATION), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DISABLE_HEADS_UP), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_HEIGHT), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -146,6 +149,8 @@ public class NotificationViewManager {
                     && !privacyMode;
             wakeOnNotification = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_WAKE_ON_NOTIFICATION, wakeOnNotification ? 1 : 0) == 1;
+            disableHeadsup = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DISABLE_HEADS_UP, disableHeadsup ? 1 : 0) == 1;
             notificationsHeight = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_HEIGHT, notificationsHeight);
             offsetTop = Settings.System.getFloat(mContext.getContentResolver(),

SeniorLimpio/frameworks_base

Conflicts:
	packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
	packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java

packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -37,11 +37,11 @@ import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.util.ldroid.QuietHoursHelper;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.android.internal.util.ldroid.QuietHoursHelper;
 
 public class NotificationViewManager {
     private final static String TAG = "Keyguard:NotificationViewManager";
@@ -77,6 +77,7 @@ public class NotificationViewManager {
         public boolean expandedView = true;
         public boolean forceExpandedView = false;
         public boolean wakeOnNotification = false;
+        public boolean disableHeadsup = false;
         public int notificationsHeight = 4;
         public float offsetTop = 0.38f;
         public boolean privacyMode = false;
@@ -106,6 +107,8 @@ public class NotificationViewManager {
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_FORCE_EXPANDED_VIEW), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_WAKE_ON_NOTIFICATION), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DISABLE_HEADS_UP), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_HEIGHT), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -146,6 +149,8 @@ public class NotificationViewManager {
                     && !privacyMode;
             wakeOnNotification = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_WAKE_ON_NOTIFICATION, wakeOnNotification ? 1 : 0) == 1;
+            disableHeadsup = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DISABLE_HEADS_UP, disableHeadsup ? 1 : 0) == 1;
             notificationsHeight = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_HEIGHT, notificationsHeight);
             offsetTop = Settings.System.getFloat(mContext.getContentResolver(),