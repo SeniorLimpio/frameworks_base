59cdbf30a39ea3bd1a0ea0f254fbfaf5dbdba56c
SeniorLimpio <seniorlimpio1@gmail.com>
2014-08-21 11:22:19 +1200

    Revert "HeadsUp: option to launch in floating window (1/2)"
    This reverts commit 7497ea6ecda371dc76ea48de05f79c29e508372f.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1318,7 +1318,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mHeadsUpNotificationView.setVisibility(View.GONE);
         mHeadsUpNotificationView.setBar(this);
-        mHeadsUpNotificationView.setNotificationHelper(mNotificationHelper);
         mHeadsUpNotificationDecay = Settings.System.getIntForUser(
                 mContext.getContentResolver(),
                 Settings.System.HEADS_UP_NOTIFCATION_DECAY,
